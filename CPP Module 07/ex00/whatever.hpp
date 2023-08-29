d17e642fdf7b390b702fc575b9c1cce7cf50b03b
Victor Chizhov <victor.chizhov93@gmail.com>
2023-08-29 13:38:14 +0400

    cpp07 ex00

---
+++ CPP Module 07/ex00/whatever.hpp
@@ -0,0 +1,10 @@
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+#include <iostream>
+
+template <typename T> void swap(T&, T&);
+template <typename T> T min(T&, T&);
+template <typename T> T max(T&, T&);
+
+#endif
